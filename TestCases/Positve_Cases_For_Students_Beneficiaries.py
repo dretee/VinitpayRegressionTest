2105ab4b8278537958a0da59c5a5d728d8d5ec99
Tony <uyahanthony008@gmail.com>
2024-09-08 20:40:33 +0100

    updates

---
+++ TestCases/Positve_Cases_For_Students_Beneficiaries.py
@@ -0,0 +1,80 @@
+# Import necessary modules and classes
+import time
+import requests
+from Utilities import ReadXyfile
+from selenium.webdriver.common.by import By
+from Utilities.RecordLogger import RecordLogger
+from Utilities.ReadProperties import ReadProperties
+from PageObject.LoginObjects import LoginObjects
+from PageObject.SignUpObjects import SignupObjects
+from PageObject.BeneficiaryObject import BeneficiaryObjects
+
+# to run the test use:   pytest -v -s TestCases/Negative_Login_Test_Case.py--browser chrome to run and also generate
+# the html report use: pytest -v -s --html=Reports\reports.html TestCases/Positive_Login_Test_Case.py --browser chrome
+"""
+Test cases for the beneficiary page
+
+Verify that a student can sign up as a beneficiary on the system (Check for the navigation when the user clicks on the student option)
+Verify the response of the system when the student uses the same information for a student who is already on the database 
+Verify the response of the system when all the filed are left empty 
+Verify that a student can be activated and deactivated after creation (Check the message given by the system for this action)
+
+"""
+
+
+class Test_Login:
+    # Initialize class variables with URLs, logger instance, and Excel file path
+    URL = ReadProperties.getTestPageURL()  # Get main page URL from configuration
+    # loginPageUrl = ReadProperties.LoginURL()  # Get login page URL from configuration
+    User_email, UserPassword = ReadProperties.getUserDetails()
+    EXISTING_EMAIL = User_email  # Get existing email from configuration
+    EXISTING_PASSWORD = UserPassword  # Get existing password
+    # from configuration
+    logger = RecordLogger.log_generator_info()  # Initialize logger instance
+
+    # Method to log the start of a test
+    def log_test_start(self, test_name):
+        self.logger.info(f"****** STARTING TEST: {test_name} ******")
+
+    # Method to log the end of a test
+    def log_test_end(self, test_name):
+        self.logger.info(f"****** ENDING TEST: {test_name} ******")
+
+    # Method to open the website
+    def open_website_and_log_in_user(self, setup, url):
+        self.log_test_start("Open Website")
+        self.driver = setup
+        self.driver.get(url)
+        self.driver.maximize_window()
+        self.Login_page_objects = LoginObjects(self.driver)
+
+        # Log in user into their account
+        self.Login_page_objects.input_email(self.EXISTING_EMAIL)
+        self.Login_page_objects.input_password(self.EXISTING_PASSWORD)
+        self.Login_page_objects.click_on_the_signin_button()
+
+        self.log_test_end("Open Website")
+
+    def test_that_a_student_can_become_beneficiary(self,setup):
+        try:
+            # Initialize Beneficiary page objects
+            self.log_test_start("Test_that_a_student_can_become_beneficiary")
+            self.open_website_and_log_in_user(setup, self.URL)
+
+            self.Beneficiary_page_objects = BeneficiaryObjects(self.driver)
+            self.Beneficiary_page_objects.click_on_the_Beneficiary_option()
+            time.sleep(2)
+            self.Beneficiary_page_objects.click_on_the_new_beneficiary_button()
+            time.sleep(3)
+            self.Beneficiary_page_objects.click_on_the_other_beneficiary_option()
+
+
+
+        except AssertionError:
+            self.logger.error("Assertion Error: this is not the page the user intends to go to.")
+            raise
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {e}")
+            raise
+        finally:
+            self.driver.quit()
