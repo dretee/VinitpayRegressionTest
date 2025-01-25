ed3ee8fa316506e54f7d815b94f395ed52dd0fb8
Tony <uyahanthony008@gmail.com>
2025-01-25 21:57:00 +0100

    Addding code for element navigation for testing the creation of businesses

--- PageObject/BusinessObjects.py
+++ PageObject/BusinessObjects.py
@@ -6,6 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 from selenium.common.exceptions import TimeoutException, NoSuchElementException
+from selenium.webdriver.support.ui import Select
 
 
 class LoginObjects:
@@ -19,6 +20,8 @@ class LoginObjects:
     category_xpath = "//div[@class='data']//div[@class='dropdown-selected']"
     description_input_xpath = "//textarea[@placeholder='tell us about your business']"
     next_button_xpath = "//button[normalize-space()='Next']"
+    search_field_xpath = "//div[@class='input-group']"
+    list_of_businesses = "//div[@class='dropdown-menu']//ul"
 
     #page after the first
     back_button_xpath = "//button[normalize-space()='Back']"
@@ -70,6 +73,112 @@ class LoginObjects:
 
         except TimeoutException:
             print(f"Sign-in button not found within {timeout} seconds")
+
+
+    def select_a_category_from_all_the_options(self, category_option, timeout=10):
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,
+
+                                 ignored_exceptions=[NoSuchElementException])
+            category_element= wait.until(ec.element_to_be_clickable((By.XPATH, self.category_xpath)))
+            category_dropdown = Select(category_element)
+            category_dropdown.select_by_visible_text(category_option)
+
+        except TimeoutException:
+            print(f"No category was found within {timeout} seconds")
+
+
+    def select_a_country_from_all_the_options(self, country_option, timeout=10):
+
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,
+                                 ignored_exceptions=[NoSuchElementException])
+            category_element= wait.until(ec.element_to_be_clickable((By.XPATH, self.Country_selection_xpath)))
+            category_dropdown = Select(category_element)
+            category_dropdown.select_by_visible_text(country_option)
+
+        except TimeoutException:
+            print(f"No country was found within {timeout} seconds")
+
+
+    def select_a_province_from_all_the_options(self, state_option, timeout=10):
+
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,
+                                 ignored_exceptions=[NoSuchElementException])
+            category_element = wait.until(ec.element_to_be_clickable((By.XPATH, self.province_selection_xpath)))
+            category_dropdown = Select(category_element)
+            category_dropdown.select_by_visible_text(state_option)
+
+        except TimeoutException:
+            print(f"No state was found within {timeout} seconds")
+
+
+    def input_the_description_text(self, input_description, timeout=10):
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,
+                                 ignored_exceptions=[NoSuchElementException])
+            description_box = wait.until(ec.element_to_be_clickable((By.XPATH, self.description_input_xpath)))
+            description_box.send_keys(input_description)
+
+        except TimeoutException:
+            print(f"No state was found within {timeout} seconds")
+
+
+    def click_on_the_next_button(self, timeout=10):
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,
+                                 ignored_exceptions=[NoSuchElementException])
+            locate_next_button = wait.until(ec.presence_of_element_located((By.XPATH, self.next_button_xpath)))
+            locate_next_button.click()
+
+        except TimeoutException:
+            print(f"The next button was not found within {timeout} seconds")
+
+
+    def input_the_address_of_the_user(self, address, timeout=10):
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,
+                                 ignored_exceptions=[NoSuchElementException])
+            address_input_field = wait.until(ec.visibility_of_element_located((By.XPATH, self.address_selection_xpath)))
+            address_input_field.send_keys(address)
+
+        except TimeoutException:
+            print(f"The address field was not found within {timeout} seconds")
+
+
+    def click_on_the_create_business_button(self, timeout=10):
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1, ignored_exceptions=[NoSuchElementException])
+            create_business_button = wait.until(ec.presence_of_element_located((By.XPATH, self.create_business_button_xpath)))
+            create_business_button.click()
+        except TimeoutException:
+            print(f"The create business button was not found within {timeout} seconds")
+
+
+    def input_name_into_the_search_field(self, search_param, timeout=10):
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,  ignored_exceptions=[NoSuchElementException])
+            username = wait.until(ec.element_to_be_clickable((By.XPATH, self.business_xpath)))
+            username.click()
+            locate_search_field = wait.until(ec.presence_of_element_located((By.XPATH, self.search_field_xpath)))
+            locate_search_field.send_keys(search_param)
+
+        except TimeoutException:
+            print(f"The search field was not found within {timeout} seconds")
+
+
+    def select_a_business_and_check(self ,name_of_business ,timeout=10):
+        try:
+            wait = WebDriverWait(self.driver, timeout=timeout, poll_frequency=1,
+                                 ignored_exceptions=[NoSuchElementException])
+            username = wait.until(ec.element_to_be_clickable((By.XPATH, self.business_xpath)))
+            username.click()
+            businesses_options = Select(self.driver.find_element(By.XPATH, self.list_of_businesses))
+            businesses_options.select_by_visible_text(name_of_business)
+
+        except TimeoutException:
+            print(f"Name was found within {timeout} seconds")
 
 
 
