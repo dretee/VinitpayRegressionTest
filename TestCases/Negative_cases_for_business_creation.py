c7366363264e06c393ca2efe0fa477be446651b2
Tony <uyahanthony008@gmail.com>
2025-02-06 14:34:55 +0100

    Addtions for the business test cases

--- TestCases/Negative_cases_for_business_creation.py
+++ TestCases/Negative_cases_for_business_creation.py
@@ -3,6 +3,8 @@ import time
 
 import pytest
 from selenium.webdriver.common.by import By
+from unicodedata import category
+
 from Utilities.RecordLogger import RecordLogger
 from Utilities.ReadProperties import ReadProperties
 from PageObject.LoginObjects import LoginObjects
@@ -56,13 +58,10 @@ class Test_Login:
 
     # input fields business names, categories and Descriptions for the first modal of  business creation
     test_data = [
-        #("", "3", "This is a business", "Please enter valid company name and description"), # Missing business name
-        ("Homeies dapping", "", "This is a business", "Please select a business category"), # Missing category
-        ("Homeies dapping", "3", "", "Please enter valid company name and description"), # Missing description
-       # ("Homeies dapping", "", "", "Please enter valid company name and description"), # Missing category and description
-        #("", "4", "", "Please enter valid company name and description"), # Missing business name and description
-        ("", "", "", "Please enter valid company name and description"), # Missing category, business and description
-        ("", "", "This is a business", "Please enter valid company name and description"),  # Missing business name and description
+        ("", "3", "This is a business", "Please enter valid company name and description"), # Missing business name
+        ("Homeies dapping", "3", "", "Please enter valid company name and description"),
+        ("", "4", "", "Please enter valid company name and description"), # Missing business name and description
+
     ]
     @pytest.mark.parametrize("business_name, category, description, error_message", test_data)
     def test_verify_that_the_user_get_a_correct_error_message_with_empty_fields(self, setup, business_name, category, description, error_message ):
@@ -77,6 +76,42 @@ class Test_Login:
             self.Business_Objects.input_the_description_text(description)
             time.sleep(4)
             self.Business_Objects.select_a_category_from_all_the_options(category)
+            time.sleep(4)
+            self.Business_Objects.click_on_the_next_button()
+            time.sleep(4)
+            error_message_text = self.driver.find_element(By.XPATH, "//div[@class='error-div']").text
+
+            assert error_message == error_message_text, self.logger.info("**** TEST FAILED: ERROR MESSAGE IS WRONG ******")
+            self.logger.info("*** TEST PASSED: ERROR MESSAGE IS CORRECT ****")
+
+        except AssertionError:
+            self.logger.error("Assertion Error: this is not the page the user intends to go to.")
+            raise
+        except Exception as e:
+            self.logger.error(f"An unexpected error occurred: {e}")
+            raise
+        finally:
+            self.driver.quit()
+
+
+    test_data = [
+    ("Homeies dapping", "This is a business", "Please select a business category"),  # Missing category
+    ("", "This is a business", "Please enter valid company name and description"),  # Missing business name and description
+    ("", "", "Please enter valid company name and description"),  # Missing category
+    ("Homeies dapping", "", "Please enter valid company name and description") # Missing category and description
+    ]
+
+    @pytest.mark.parametrize("business_name, description, error_message", test_data)
+    def test_verify_that_the_category_field_when_missing_the_correct_error_message_is_given(self,setup, business_name, description, error_message):
+        try:
+            # Initialize Beneficiary page objects
+            self.log_test_start("")
+            self.open_website_and_log_in_user(setup, self.URL)
+            self.Business_Objects = BusinessObjects(self.driver)
+            self.Business_Objects.locate_and_click_username_and_add_business_button()
+
+            self.driver.find_element(By.XPATH, "//form/div[1]/div/input").send_keys(business_name)
+            self.Business_Objects.input_the_description_text(description)
             time.sleep(4)
             self.Business_Objects.click_on_the_next_button()
             time.sleep(4)
